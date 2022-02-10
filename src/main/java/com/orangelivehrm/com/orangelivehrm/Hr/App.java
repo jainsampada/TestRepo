3a827010210d5554a21f666b9fa002c5dc0a7321
jainsampada <jain.sampada@gmail.com>
2022-02-10 15:18:59 +0530

    First Version

---
+++ src/main/java/com/orangelivehrm/com/orangelivehrm/Hr/App.java
@@ -0,0 +1,13 @@
+package com.orangelivehrm.com.orangelivehrm.Hr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
